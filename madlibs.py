9beea4338bf70df8ad3c607638d2b76a728d59f6
Dihfahsih1 <dihfahsihm@gmail.com>
2021-02-09 10:48:30 +0300

    first commit

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+verb1 = input("Enter the verb: ")
+verb2 = input("Enter the verb: ")
+action = input("Enter the action: ")
+
+madlib = f'programming is so {verb1} but it becomes a {verb2} if you over do it \
+with out {action}'
+print(madlib)
