cef5d7da4a97bf5accf87fa6a1abeb07514e5980
Dihfahsih1 <dihfahsihm@gmail.com>
2021-06-18 23:32:11 +0300

    no coding

--- intermediate_level/lambda.py
+++ intermediate_level/lambda.py
@@ -1,15 +1,12 @@
 #lambda function
 
-#ordinary function
 def func(x):
     return x+5
 print(func(2))
 
-#using lambda
 func2 = lambda x: x+5
 print(func2(3))
 
-#Filtering a list using lambda
 a=[1,2,3,4,5,6,7,8,9,10]
 
 def func3(x):
