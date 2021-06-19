351724996e277973cdf38c92d8ac637ed943fca7
Dihfahsih1 <dihfahsihm@gmail.com>
2021-06-19 21:09:14 +0300

    changes

--- guess_game.py
+++ guess_game.py
@@ -1,17 +1,5 @@
 import random    
 
-# def guess(x):
-#     random_number = random.randint(1, x)
-#     guess = 0
-#     while guess != random_number:
-#         guess = int(input(f'Guess a number between 1 and {x}: '))
-#         if guess < random_number:
-#             print('Sorry, guess again. Too low.')
-#         elif guess > random_number:
-#             print('Sorry, guess again. Too high')
-#     print(f'Congrats, you have guessed the right number {random_number} correctly !!!')
-# guess(10)
-
 def computer_guess(x):
     low = 1
     high = x
