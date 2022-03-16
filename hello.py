0c154fb0cb46ed1f23e973bafffa5b008623f6bf
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-16 06:49:09 +0300

    implement slicing

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("hello world")
 grade= 70
 if grade >=70:
+    pass
---
+++ slicing.py
@@ -0,0 +1,7 @@
+url1 = "http://gmail.com"
+url2 = "http://google.com"
+
+slice = slice(7,-4)
+
+print(url1[slice])
+print(url2[slice])
